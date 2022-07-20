f6e141f21a0ea531fda29eb0459139a619da6992
nheeb <nitai64@yahoo.de>
2022-07-20 16:52:17 +0200

    Interact Ball

--- Actors/Player.gd
+++ Actors/Player.gd
@@ -1,11 +1,15 @@
 extends GroundedPhysicsMover3D
 
+var facing_direction := Vector3.RIGHT
+
 func _physics_process(delta):
 	var move_direction := Vector3.ZERO
 	move_direction.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	move_direction.z = Input.get_action_strength("move_back") - Input.get_action_strength("move_forward")
-	
-	move_direction = move_direction.normalized()
 
-	add_acceleration(ACC * move_direction)
+	var move_direction_normalized := move_direction.normalized()
+	if move_direction.length() > .1:
+		facing_direction = move_direction_normalized
+
+	add_acceleration(ACC * move_direction_normalized)
 	execute_movement(delta)
---
+++ Objects/PickUpBall.gd
@@ -0,0 +1,7 @@
+extends Spatial
+
+func set_hover_state(active: bool, actor):
+	pass
+
+func on_interact(actor):
+	pass
