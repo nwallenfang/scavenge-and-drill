6fe0de0403a06940d906f92b9413ed919e935754
nwallenfang <nils@wallenfang.de>
2022-08-01 22:09:22 +0200

    add some icons

--- UI/DialogPanel.gd
+++ UI/DialogPanel.gd
@@ -6,13 +6,14 @@ func on_interrupt():
 	$Timer.stop()
 
 
-
-
 signal line_done_naturally
 func show_dialog_line(line):
 	Game.log("show line " + line.text)
 	# TODO add color
-	$"%Name".bbcode_text = "[center]" + Dialog.names[line.speaker_id]
+	var color_hex = Dialog.colors[line.speaker_id].to_html()
+	var color_bb = "[color=#%s]" % color_hex
+	$"%Name".bbcode_text = "[center]" + color_bb + Dialog.names[line.speaker_id]
+	# maybe make the text the same color? don't know
 	$"%Text".text = line.text
 	$"%Icon".material.set("shader_param/texture_resource", Dialog.icons[line.speaker_id])
 	$Timer.start(line.duration)
