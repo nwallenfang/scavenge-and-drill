fa8fa6de95382685f556904f1a95a39b0b652207
nheeb <nitai64@yahoo.de>
2022-08-03 18:40:49 +0200

    speed mode done

--- Actors/Drill.gd
+++ Actors/Drill.gd
@@ -3,8 +3,23 @@ extends Player
 export var move_acc_default := 120.0
 export var move_acc_upgraded := 130.0
 
+var mounted := false setget set_mounted
+
+func set_mounted(m):
+	mounted = m
+	$CollisionShape.disabled = m
+
 func _physics_process(delta):
+	if mounted:
+		self.global_2d = Game.roller.global_2d
+		return
 	._physics_process(delta)
 	if controlled:
 		if Input.is_action_just_pressed("initiate_swap"):
 			Game.rpc("try_swap", name)
+		if Input.is_action_just_pressed("super_mode"):
+			Game.rpc("try_super", name)
+
+func _network_process(delta):
+	if not mounted:
+		._network_process(delta)
