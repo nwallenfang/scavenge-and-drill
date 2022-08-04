806857d9f6e41ae186f156cad2773bd749fc5028
nwallenfang <nils@wallenfang.de>
2022-08-04 16:11:57 +0200

    fix bullet raising yield error

--- Actors/Bullet.gd
+++ Actors/Bullet.gd
@@ -7,8 +7,8 @@ var direction : Vector3
 func _ready():
 	if Game.upgrades.more_bullet_damage:
 		$BulletModel.scale *= 1.5
-#	yield(get_tree().create_timer(3.0),"timeout")
-#	queue_free()
+	yield(get_tree().create_timer(3.0),"timeout")
+	queue_free()
 
 func _network_init(data):
 	var direction_2d = data["direction"]
@@ -21,8 +21,12 @@ func _physics_process(delta):
 
 
 func _on_Area_area_entered(area):
-	queue_free()
+	visible = false
+	$Area.set_deferred("monitoring", false)
+	$Area.set_deferred("monitorable", false)
 
 
 func _on_Area_body_entered(body):
-	queue_free()
+	visible = false
+	$Area.set_deferred("monitoring", false)
+	$Area.set_deferred("monitorable", false)
