933f986647c285ea863fe52a399ab1768bbfab59
nheeb <nitai64@yahoo.de>
2022-08-04 13:23:22 +0200

    Scrap Models

--- Objects/Pickup/Treasure.gd
+++ Objects/Pickup/Treasure.gd
@@ -4,7 +4,7 @@ extends Spatial
 export var version := 1 setget set_version
 func set_version(v):
 	version = v
-	$TreasureModel.set_version(v)
+	$Model.set_version(v)
 
 func _on_TreasureArea_picked_up() -> void:
 	# TODO maybe blend out
