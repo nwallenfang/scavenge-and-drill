58f7d869d3d6c7499548ee9192e693649e0be96c
nwallenfang <nils@wallenfang.de>
2022-08-04 19:15:11 +0200

    building the level begin

--- Objects/Pickup/EnergyCrystal.gd
+++ Objects/Pickup/EnergyCrystal.gd
@@ -10,7 +10,7 @@ func get_drilled(amount):
 	hp -= amount
 	$Model.scale.y = lerp(.3, 1.0, hp)
 	if Game.host and hp <= 0.0:
-		Game.rpc("sync_energy_charges", Game.energy_charges + 1.0)
+		Game.rpc("sync_energy_charges", Game.energy_charges + 2.0)
 		rpc("drilled_out")
 
 remotesync func drilled_out():
