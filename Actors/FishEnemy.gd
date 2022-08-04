22449b46eb2dc617147d330edb951936df619500
nheeb <nitai64@yahoo.de>
2022-08-04 23:03:04 +0200

    Eel Damage Red Animation

--- Actors/FishEnemy.gd
+++ Actors/FishEnemy.gd
@@ -7,7 +7,7 @@ var path_target_position : Vector3
 var path_direction := 1
 var path_speed := 1.5
 var current_path_offset := 0.0
-var hp := 3
+var hp := 2
 var can_attack := false
 var attack_speed := 2.5
 var target: Spatial
@@ -62,6 +62,7 @@ func _on_Hurtbox_area_entered(area):
 
 remotesync func on_getting_hit():
 	hp -= 3 if Game.upgrades.more_bullet_damage else 1
+	$Model/EelModel.hurt_animation()
 	if hp == 0:
 		queue_free()
 
--- Assets/Models/EelModel.gd
+++ Assets/Models/EelModel.gd
@@ -20,3 +20,9 @@ func _process(delta):
 	if sync_bones_with_spatials:
 		for i in $Skeleton.get_bone_count():
 			$Skeleton.set_bone_pose(i, get_node("Bones/" + str(i)).transform)
+
+func hurt_animation():
+	$Skeleton/EelBody.material_overlay.albedo_color.a = 1.0
+	get_tree().create_tween().tween_property($Skeleton/EelBody.material_overlay, "albedo_color:a", 0.0, .8)
+	$Skeleton/EelKamm.material_overlay.albedo_color.a = 1.0
+	get_tree().create_tween().tween_property($Skeleton/EelKamm.material_overlay, "albedo_color:a", 0.0, .8)
