0d1383be45b44e02498e123b7f12e082992c6ed7
nheeb <nitai64@yahoo.de>
2022-08-05 15:38:42 +0200

    Drill Animation

--- Objects/Pickup/Diamond.gd
+++ Objects/Pickup/Diamond.gd
@@ -1,7 +1,19 @@
 extends Spatial
 
+var currently_drilled := false
 var hp := 1.0
 func get_drilled(delta):
+	currently_drilled = true
 	hp -= delta
+	$DrillTarget.translation.y = lerp(1.5, 2.1, hp)
+	$Pivot.scale = Vector3.ONE * lerp(.8, 1.0, hp)
+	if not $DrillParticles.emitting:
+		$DrillParticles.emitting = true
 	if hp <= 0.0:
 		Game.drill.cooldown()
+
+func _physics_process(delta):
+	if currently_drilled:
+		if not Game.drill.is_drilling:
+			currently_drilled = false
+			$DrillParticles.emitting = false
